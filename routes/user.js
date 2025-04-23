1af15b452ff206965e13ca62119c5bf55d9f86e1
Mandus Lindström <mandus85@hotmail.com>
2025-04-23 10:49:19 +0200

    done with code along before correct example data from swagger hub

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,12 @@ import express from "express";
 
 const router = express.Router();
 
-// swagger info om GET anropet
+let users = [
+  { id: 1, name: "Mandus" },
+  { id: 2, name: "Hubert" },
+];
+
+// Swagger info: GET / behöver vi dokumentera
 
 /**
  * @swagger
@@ -14,7 +19,7 @@ const router = express.Router();
  *     description: Returnerar en lista med alla användare
  *     responses:
  *       200:
- *         description: Lyckad hämtning av användare
+ *         description: Lyckad hämtning av alla användare
  *         content:
  *           application/json:
  *             schema:
@@ -24,23 +29,17 @@ const router = express.Router();
  *                 properties:
  *                   id:
  *                     type: integer
- *                     description: Användarens unika ID
+ *                     description: Användarens unika id
  *                   name:
  *                     type: string
  *                     description: Användarens namn
  */
 
-// skapa en GET route för att hämta alla users
 router.get("/", (req, res) => {
-  const users = [
-    { id: 1, name: "Mandus" },
-    { id: 2, name: "Klas" },
-  ];
-
-  res.json(users);
+  res.status(200).json(users);
 });
 
-// swagger info om POST anropet
+// Swagger info: POST / behöver vi dokumentera
 
 /**
  * @swagger
@@ -49,39 +48,28 @@ router.get("/", (req, res) => {
  *     tags:
  *       - users
  *     summary: Skapa en ny användare
- *     description: Skapar en användare med ett namn
+ *     description: Skapar en ny användare
  *     requestBody:
  *       required: true
  *       content:
  *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               name:
- *                 type: string
- *                 description: Namnet på användaren
- *                 example: "Kristina"
+ *            schema:
+ *             $ref: '#/components/schemas/User'
  *     responses:
  *       201:
  *         description: Användare skapad
  *         content:
  *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   description: Bekräftelsemeddelande
- *                 user:
- *                   type: object
- *                   description: Den skapade användaren
+ *            schema:
+ *               $ref: '#/components/schemas/UserResponse'
  */
 
-// Skapa en POST route för att kunna skapa en ny user
 router.post("/", (req, res) => {
   const newUser = req.body;
 
-  res.status(201).json({ message: "Användaren är skapad", user: newUser });
+  users.push(newUser);
+
+  res.status(201).json({ message: "Användare skapad", user: newUser });
 });
 
 export default router;
